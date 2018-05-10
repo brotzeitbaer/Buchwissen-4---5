fdc101f3463082220b5d8fe0fb441e2a07f47a11
Michael k <michaelkahnt123@gmail.com>
2018-05-10 14:46:42 +0200

    Update 5.9 strukturen.cpp

--- 5.9 strukturen.cpp
+++ 5.9 strukturen.cpp
@@ -5,16 +5,20 @@ using namespace std;
 int main()
 {
 	
-	struct s_spieler
+	struct s_spieler // mit dem "struct" befehl ist es möglich variablen und arrays in gruppen zusammen zu fassen.
 	{
 		int energie;
 		char name[30];
 		
 	};
+	// mit dem zusammenfassen wird es übersichtlicher bei vielen variablen
+	
 	
 	s_spieler spieler1;
 	s_spieler spieler2;
 	s_spieler spieler3;
+	// die struct variable kann so oft vervielfältigt werden wie man möchte
+	
 	
 	cout << "spieler 1 name eingeben   : \n";
 	cin.get (spieler1.name, 29);
@@ -23,6 +27,8 @@ int main()
 	cin  >> spieler1.energie;
 	
 	cin.ignore();
+	//der cin.ignore befehl setzt die letzte eingabe wieder auf anfang.
+	
 	
 	cout << "spieler 2 name eingeben   : \n";
 	cin.get (spieler2.name, 29);
@@ -40,7 +46,7 @@ int main()
 	
 	cout << endl;
 	
-	cout << "spieler 1 name:    "  << spieler1.name    << endl
+	cout  	 << "spieler 1 name:    "  << spieler1.name    << endl
 		 << "spieler 1 energie: "  << spieler1.energie << endl
 		 << "spieler 2 name:    "  << spieler2.name    << endl
 		 << "spieler 2 energie: "  << spieler2.energie << endl
