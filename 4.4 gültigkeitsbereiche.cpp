e890023a0a7ed1cec50197761f0f9717e79b7efa
Michael k <michaelkahnt123@gmail.com>
2018-05-10 14:40:53 +0200

    Rename 4.4.gültigkeitsbereiche.cpp to 4.4 gültigkeitsbereiche.cpp

--- 4.4 gültigkeitsbereiche.cpp
+++ 4.4 gültigkeitsbereiche.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int berechnepunkte();
-int bonus = 500; /*diese variable ist außerhalb der funktionen deklariert und somit eine "globale variable".
-der unterscheid zu einer lokalen ist das die globale überall gültig ist und nicht nur in einem bestimmten codeblock.*/
+int bonus = 500; /*diese variable ist auÃŸerhalb der funktionen deklariert und somit eine "globale variable".
+der unterscheid zu einer lokalen ist das die globale Ã¼berall gÃ¼ltig ist und nicht nur in einem bestimmten codeblock.*/
 
 int main()
 {
 	
 	int punkte; /*diese variable ist NICHT die selbe wie die in der funktion "brechnepunkte
-	eine variable ist dort gültig wo sie deklariert wurde. das hier ist eine sogenannte "lokale variable". */
+	eine variable ist dort gÃ¼ltig wo sie deklariert wurde. das hier ist eine sogenannte "lokale variable". */
 	
 	punkte = berechnepunkte();
 	
@@ -29,5 +29,5 @@ berechnepunkte()
 	cout << "punkte eingeben\n";
 	cin >> punkte;
 	
-	return punkte; //den wert zurück geben
+	return punkte; //den wert zurÃ¼ck geben
 }
