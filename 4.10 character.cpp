85118a6665c08949bf2ae41082bc583b96e77301
Michael k <michaelkahnt123@gmail.com>
2020-01-29 20:05:59 +0100

    Update and rename 4.10 charakter.cpp to 4.10 character.cpp

---
+++ 4.10 character.cpp
@@ -0,0 +1,15 @@
+#include "character.hpp"
+
+
+int berechnen(int strengh, int factor)
+{
+	
+	return strengh = strengh + (strengh * factor) / 100;
+	
+}
+float berechnen(float fspeed, float ffactor)
+{
+
+	return fspeed = fspeed + (fspeed * ffactor) / 100.0f;
+
+}
